babfcbdc6808f8dc04acb47aaa2324569346e864
Yuyang(Kris) Wang <92161695+yuywang9@users.noreply.github.com>
2026-04-21 21:49:30 +0800

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,77 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # encoded = ""
+        # for s in strs:
+        #     encoded += f"{len(s)}:{s}"
+        #     # We need to add : because what if the len number is multiple digits? How we identify the len with the numbers in the word? 
+        # return encoded
+
+
+
+
+        encoded = ""
+        for s in strs:
+            encoded += f"{len(s)}#{s}"
+        
+        return encoded
+
+
+
+
+
+    def decode(self, s: str) -> List[str]:
+        # res = []
+        # i = 0
+        # while i < len(s):
+        #     j = i 
+        #     while s[j] != ":":
+        #         j += 1
+        #     # now j is at #
+        #     length = int(s[i : j])
+        #     word = s[j + 1 : j + 1 + length]
+        #     res.append(word)
+        #     #update i
+        #     i = j + 1 + length
+        # return res
+        
+
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i : j])
+
+            res.append(s[j + 1 : j + 1 + length])
+
+            i = j + 1 + length
+        
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
